332dbaf314ee9f5b3bfe09b6678de95a43255742
JakeAnder55 <56042125+JakeAnder55@users.noreply.github.com>
2019-11-07 16:40:56 -0600

    Add files via upload

--- BinarySearchTreeClient.java
+++ BinarySearchTreeClient.java
@@ -2,17 +2,21 @@
  *
  * @author ix6638ym
  */
+import java.io.File; 
+import java.io.FileNotFoundException;
+import java.util.Scanner; 
+
 public class BinarySearchTreeClient {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // TODO code application logic here
         
-        BinarySearchTree<Integer> myTree = new BinarySearchTree<Integer>();
+        BinarySearchTree<String> myTree = new BinarySearchTree<String>();
         
-        myTree.insertNode(29);
+      /*  myTree.insertNode(29);
         myTree.insertNode(18);
         myTree.insertNode(5);
         myTree.insertNode(34);
@@ -21,8 +25,14 @@ public class BinarySearchTreeClient {
         myTree.insertNode(75);
         myTree.insertNode(66);
         myTree.insertNode(24);
-        myTree.insertNode(50);
-        
+        myTree.insertNode(50); */
+         // pass the path to the file as a parameter 
+    File file = new File("C:\\temp\\chatdictionary.txt"); 
+    Scanner sc = new Scanner(file); 
+  
+    while (sc.hasNextLine()) 
+      System.out.println(sc.nextLine()); 
+          
         System.out.println("Inorder: ");
         myTree.inorderTraverse();
         
@@ -32,7 +42,7 @@ public class BinarySearchTreeClient {
         System.out.println("\nPostorder: ");
         myTree.postorderTraverse();
         
-        TreeNode temp = myTree.treeSearch(myTree.getRoot(), 75);
+        TreeNode temp = myTree.treeSearch(myTree.getRoot(), ".exe");
         
         if(temp == null) {
             System.out.println("\n\nNot Found");
@@ -53,5 +63,6 @@ public class BinarySearchTreeClient {
             }
         }
     }
-    
+ 
+ 
 }
